55a360c32c90314fd9178575170567b7230aeae2
Leo Dion <leogdion@brightdigit.com>
2016-07-02 21:07:09 -0400

    fixed swift3 update

--- swiftver/rfc3339.swift
+++ swiftver/rfc3339.swift
@@ -5,7 +5,7 @@ import Foundation
 ///
 /// :param: rfc3339DateTimeString string with format "yyyy-MM-ddTHH:mm:ssZ"
 /// :returns: NSDate, or nil if string cannot be parsed
-public func date(forRFC3339DateTimeString rfc3339DateTimeString: String) -> NSDate? {
+public func date(forRFC3339DateTimeString rfc3339DateTimeString: String) -> Date? {
     let formatter = getThreadLocalRFC3339DateFormatter()
     return formatter.date(from: rfc3339DateTimeString)
 }
@@ -14,14 +14,14 @@ public func date(forRFC3339DateTimeString rfc3339DateTimeString: String) -> NSDa
 ///
 /// :param: date NSDate
 /// :returns: String
-public func rfc3339DateTimeStringForDate(date: Date) -> String {
+public func rfc3339DateTimeStringForDate(_ date: Date) -> String {
     let formatter = getThreadLocalRFC3339DateFormatter()
     return formatter.string(from: date)
 }
 
 // Date formatters are not thread-safe, so use a thread-local instance
 private func getThreadLocalRFC3339DateFormatter() -> DateFormatter {
-    return cachedThreadLocalObjectWithKey(key: "net.kristopherjohnson.getThreadLocalRFC3339DateFormatter") {
+    return cachedThreadLocalObjectWithKey("net.kristopherjohnson.getThreadLocalRFC3339DateFormatter") {
         let en_US_POSIX = Locale(localeIdentifier: "en_US_POSIX")
         let rfc3339DateFormatter = DateFormatter()
         rfc3339DateFormatter.locale = en_US_POSIX
@@ -35,7 +35,7 @@ private func getThreadLocalRFC3339DateFormatter() -> DateFormatter {
 ///
 /// :param: create closure that will be invoked to create the object
 /// :returns: object of type T
-private func cachedThreadLocalObjectWithKey<T: AnyObject>(key: String, create: () -> T) -> T {
+private func cachedThreadLocalObjectWithKey<T: AnyObject>(_ key: String, create: () -> T) -> T {
     let threadDictionary = Thread.current().threadDictionary
         if let cachedObject = threadDictionary[key] as? T {
             return cachedObject
