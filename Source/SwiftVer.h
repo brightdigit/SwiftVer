80ee09f6088929dfae65bb2b651d0c4f2c23f7d8
Leo Dion <leogdion@brightdigit.com>
2016-09-20 21:09:07 -0400

    added mac and watch framework

--- Source/SwiftVer.h
+++ Source/SwiftVer.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 BrightDigit, LLC. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftVer.
 FOUNDATION_EXPORT double SwiftVerVersionNumber;
