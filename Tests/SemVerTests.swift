a22b36ca8da1e3c82a8ccb01991152a9fef6bb91
Leo Dion <leogdion@brightdigit.com>
2016-09-21 11:12:34 -0400

    organized Examples

--- Tests/SemVerTests.swift
+++ Tests/SemVerTests.swift
@@ -53,16 +53,4 @@ class SemVerTests: XCTestCase {
     XCTAssertNil(semver?.description)
   }
   
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-  }
-  
-  func testPerformanceExample() {
-    // This is an example of a performance test case.
-    self.measure {
-      // Put the code you want to measure the time of here.
-    }
-  }
-  
 }
