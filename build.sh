8e39f1aea78251d6d93c8d1a63848ca8a30dbf4c
Leo Dion <leogdion@brightdigit.com>
2016-12-02 09:45:11 -0500

    added project parameter

--- build.sh
+++ build.sh
@@ -20,8 +20,8 @@ iOSVersions_LOCAL=(8.4 9.3 10.1)
 [[ $TRAVIS ]] && iOSVersions=${iOSVersions_TRAVIS[*]} || iOSVersions=${iOSVersions_LOCAL[*]}
 
 for iOSVersion in ${iOSVersions[@]}; do
-	xcodebuild -scheme "SwiftVer iOS" -destination "platform=iOS Simulator,name=iPhone 5,OS=${iOSVersion}" clean test
-	xcodebuild -scheme "SwiftVer iOS" -destination "platform=iOS Simulator,name=iPad Air,OS=${iOSVersion}" clean test
+	xcodebuild -project SwiftVer.xcodeproj -scheme "SwiftVer iOS" -destination "platform=iOS Simulator,name=iPhone 5,OS=${iOSVersion}" clean test
+	xcodebuild -project SwiftVer.xcodeproj -scheme "SwiftVer iOS" -destination "platform=iOS Simulator,name=iPad Air,OS=${iOSVersion}" clean test
 done
 
 tvOSVersions_TRAVIS=(9.0 9.1 9.2 10.0)
@@ -30,7 +30,7 @@ tvOSVersions_LOCAL=(10.0)
 [[ $TRAVIS ]] && tvOSVersions=${tvOSVersions_TRAVIS[*]} || iOSVersions=${tvOSVersions_LOCAL[*]}
 
 for tvOSVersion in ${tvOSVersions[@]}; do
-	xcodebuild -scheme "SwiftVer tvOS" -destination "platform=tvOS Simulator,name=Apple TV 1080p,OS=${tvOSVersion}" clean test
+	xcodebuild -project SwiftVer.xcodeproj -scheme "SwiftVer tvOS" -destination "platform=tvOS Simulator,name=Apple TV 1080p,OS=${tvOSVersion}" clean test
 done
 
-xcodebuild -scheme "SwiftVer macOS" -destination 'platform=OS X,arch=x86_64' clean test
+xcodebuild -project SwiftVer.xcodeproj -scheme "SwiftVer macOS" -destination 'platform=OS X,arch=x86_64' clean test
