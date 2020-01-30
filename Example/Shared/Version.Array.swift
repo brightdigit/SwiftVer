f445dfe1ac5423782a479d135b389512a4d05cbb
Leo Dion <leogdion@brightdigit.com>
2020-01-30 14:37:09 -0500

    fixing example project

--- Example/Shared/Version.Array.swift
+++ Example/Shared/Version.Array.swift
@@ -3,19 +3,19 @@ import SwiftVer
 
 let exampleVersion = Version(
   bundle: Bundle.main,
+  dictionary: Stage.dictionary(fromPlistAtURL: Bundle.main.url(forResource: "versions", withExtension: "plist")!)!,
   versionControl: VersionControlInfo(
-    TYPE: VCS_TYPE,
-    BASENAME: VCS_BASENAME,
-    UUID: VCS_UUID,
-    NUM: VCS_NUM,
-    DATE: VCS_DATE,
-    BRANCH: VCS_BRANCH,
-    TAG: VCS_TAG,
-    TICK: VCS_TICK,
-    EXTRA: VCS_EXTRA,
-    FULL_HASH: VCS_FULL_HASH,
-    SHORT_HASH: VCS_SHORT_HASH,
-    WC_MODIFIED: VCS_WC_MODIFIED
+    type: VCS_TYPE,
+    baseName: VCS_BASENAME,
+    uuid: Hash(string: VCS_UUID!)!,
+    number: VCS_NUM,
+    date: VCS_DATE,
+    branch: VCS_BRANCH,
+    tag: VCS_TAG,
+    tick: VCS_TICK,
+    extra: VCS_EXTRA,
+    hash: Hash(string: VCS_FULL_HASH)!,
+    isWorkingCopyModified: VCS_WC_MODIFIED
   )
 )
 
@@ -27,22 +27,21 @@ extension Version {
     values.append((label: "semverMinor", value: semver.minor.description))
     values.append((label: "semverPatch", value: semver.patch?.description))
     values.append((label: "build", value: build.description))
-    values.append((label: "vcsBasename", value: versionControl!.BASENAME))
-    values.append((label: "vcsBranch", value: versionControl!.BRANCH))
-    values.append((label: "vcsDate", value: versionControl!.DATE?.description))
-    values.append((label: "vcsExtra", value: versionControl!.EXTRA))
-    values.append((label: "vcsFull_hash", value: versionControl!.FULL_HASH))
-    values.append((label: "vcsNum", value: versionControl!.NUM.description))
-    values.append((label: "vcsShortHash", value: versionControl!.SHORT_HASH))
-    values.append((label: "vcsTag", value: versionControl!.TAG))
-    values.append((label: "vcsTick", value: versionControl!.TICK?.description))
-    values.append((label: "vcsType", value: versionControl!.TYPE.description))
-    values.append((label: "vcsUuid", value: versionControl!.UUID))
-    values.append((label: "vcsWc_modified", value: versionControl!.WC_MODIFIED.description))
+    values.append((label: "vcsBasename", value: versionControl!.baseName))
+    values.append((label: "vcsBranch", value: versionControl!.branch))
+    values.append((label: "vcsDate", value: versionControl!.date?.description))
+    values.append((label: "vcsExtra", value: versionControl!.extra))
+    values.append((label: "vcsFull_hash", value: versionControl!.hash.description))
+    values.append((label: "vcsNum", value: versionControl!.number.description))
+    values.append((label: "vcsTag", value: versionControl!.tag))
+    values.append((label: "vcsTick", value: versionControl!.tick?.description))
+    values.append((label: "vcsType", value: versionControl!.type.description))
+    values.append((label: "vcsUuid", value: versionControl!.uuid?.description))
+    values.append((label: "vcsWc_modified", value: versionControl!.isWorkingCopyModified.description))
     return values
   }
 
   static var example: Version! {
-    return _exampleVersion
+    return exampleVersion
   }
 }
--- Example/Source/AppDelegate.swift
+++ Example/Source/AppDelegate.swift
@@ -3,9 +3,4 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
-
-  func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
-  }
 }
