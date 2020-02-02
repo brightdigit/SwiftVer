c1d13816b25b92c669e143192b50a41c82fb8c5a
Leo Dion <leogdion@brightdigit.com>
2020-02-02 15:02:08 -0500

    fixing install

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -8,13 +8,14 @@ else
   #sudo apt-get install clang libicu-dev llvm
   #sudo eval "$(curl -sL https://swiftenv.fuller.li/install.sh)"
   # What to do in Ubunutu
-  sudo apt-get install clang libicu-dev
+  #sudo apt-get install clang libicu-dev
   wget -q -O - https://swift.org/keys/all-keys.asc | \
   gpg --import -
   gpg --keyserver hkp://pool.sks-keyservers.net --refresh-keys Swift
-
+  wget https://swift.org/builds/swift-5.1.3-release/ubuntu1804/swift-5.1.3-RELEASE/swift-5.1.3-RELEASE-ubuntu18.04.tar.gz.sig
   wget https://swift.org/builds/swift-5.1.3-release/ubuntu1804/swift-5.1.3-RELEASE/swift-5.1.3-RELEASE-ubuntu18.04.tar.gz
   gpg --verify swift-5.1.3-RELEASE-ubuntu18.04.tar.gz.sig
   tar xzf swift-5.1.3-RELEASE-ubuntu18.04.tar.gz
+  ls "${PWD}/usr/bin"
   export PATH="${PWD}/usr/bin:${PATH}"
 fi
