0d394373923f40c149ee4f1db3b9f925e6c465e3
Leo Dion <leogdion@brightdigit.com>
2020-02-02 14:31:12 -0500

    installing swift on linux

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -4,7 +4,8 @@ if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   brew update
   brew bundle
 else
-  sudo apt install clang llvm
+  #sudo apt install clang llvm
+  #sudo apt-get install clang libicu-dev llvm
   eval "$(curl -sL https://swiftenv.fuller.li/install.sh)"
   # What to do in Ubunutu
 fi
