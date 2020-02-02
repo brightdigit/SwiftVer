45366ed1f7cbd7aa2a420b6b955e5bb908cbde9f
Leo Dion <leogdion@brightdigit.com>
2020-02-02 14:48:13 -0500

    working on swift installation

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -6,6 +6,15 @@ if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
 else
   #sudo apt install clang llvm
   #sudo apt-get install clang libicu-dev llvm
-  sudo eval "$(curl -sL https://swiftenv.fuller.li/install.sh)"
+  #sudo eval "$(curl -sL https://swiftenv.fuller.li/install.sh)"
   # What to do in Ubunutu
+  sudo apt-get install clang libicu-dev
+  wget -q -O - https://swift.org/keys/all-keys.asc | \
+  gpg --import -
+  gpg --keyserver hkp://pool.sks-keyservers.net --refresh-keys Swift
+
+  wget https://swift.org/builds/swift-5.1.3-release/ubuntu1804/swift-5.1.3-RELEASE/swift-5.1.3-RELEASE-ubuntu18.04.tar.gz
+  gpg --verify swift-5.1.3-RELEASE-ubuntu18.04.tar.gz.sig
+  tar xzf swift-5.1.3-RELEASE-ubuntu18.04.tar.gz
+  export PATH="${PWD}/usr/bin:"${PATH}"
 fi
