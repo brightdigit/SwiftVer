d7b4729d25814b9b5982ef47eb182ad7d39c78c8
leogdion <leogdion@brightdigit.com>
2020-02-03 16:44:25 -0500

    Update script.sh

--- Scripts/script.sh
+++ Scripts/script.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
-  pod lib lint
   swiftformat --lint . && swiftlint
-  swift build
-  swift test  --enable-code-coverage
+else
+  # What to do in Ubunutu
+  export PATH="${PWD}/swift-5.1.3-RELEASE-ubuntu18.04/usr/bin:$PATH"
+fi
+
+swift build
+swift test  --enable-code-coverage
+
+if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   xcrun llvm-cov export -format="lcov" .build/debug/swiftverPackageTests.xctest/Contents/MacOS/SwiftVerPackageTests -instr-profile .build/debug/codecov/default.profdata > info.lcov
   bash <(curl https://codecov.io/bash) -F travis -F macOS -n $TRAVIS_JOB_NUMBER-$TRAVIS_OS_NAME
+else
+  llvm-cov export -format="lcov" .build/x86_64-unknown-linux/debug/SwiftVerPackageTests.xctest -instr-profile .build/debug/codecov/default.profdata > info.lcov
+  bash <(curl https://codecov.io/bash) -F travis -F bionic -n $TRAVIS_JOB_NUMBER-$TRAVIS_OS_NAME
+fi
+
+if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+  pod lib lint
   swift package generate-xcodeproj
   pod install --project-directory=Example
   ./Example/Shared/autorevision/autorevision.sh -t swift > Example/Source/VCS.swift
   xcodebuild -workspace Example/Examples.xcworkspace -scheme "iOS Example"  ONLY_ACTIVE_ARCH=NO  CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO  CODE_SIGNING_ALLOWED=NO
   xcodebuild -workspace Example/Examples.xcworkspace -scheme "tvOS Example"  ONLY_ACTIVE_ARCH=NO   CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO  CODE_SIGNING_ALLOWED=NO
   xcodebuild -workspace Example/Examples.xcworkspace -scheme "macOS Example"  ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO  CODE_SIGNING_ALLOWED=NO
-else
-  # What to do in Ubunutu
-  export PATH="${PWD}/swift-5.1.3-RELEASE-ubuntu18.04/usr/bin:$PATH"
-  swift build 
-  swift test --enable-code-coverage
-  llvm-cov export -format="lcov" .build/x86_64-unknown-linux/debug/SwiftVerPackageTests.xctest -instr-profile .build/debug/codecov/default.profdata > info.lcov
-  bash <(curl https://codecov.io/bash) -F travis -F bionic -n $TRAVIS_JOB_NUMBER-$TRAVIS_OS_NAME
 fi
