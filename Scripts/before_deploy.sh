26f2f80ac35c8ced67fdf8940cc69a06fb45bc78
leogdion <leogdion@brightdigit.com>
2020-02-03 16:41:00 -0500

    [ci skip] removing before deploy

--- Scripts/before_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
-#  carthage build --no-skip-current
-#  carthage archive $FRAMEWORK_NAME
-#else
-  # What to do in Ubunutu
-#  echo "test"
-#fi
