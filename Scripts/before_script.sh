b2e3e14c9f69b2865ee1b15ae0ed06e17c295244
Leo Dion <leogdion@brightdigit.com>
2020-02-03 11:12:32 -0500

    Revert "adding travis-ci codeclimate coverage"
    This reverts commit 42340d091a6c86b2029ed372c45b7cd5f31b7c20.

--- Scripts/before_script.sh
+++ Scripts/before_script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x ./cc-test-reporter
-./cc-test-reporter before-build
-
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+  
   swift package generate-xcodeproj
 else
   # What to do in Ubunutu
